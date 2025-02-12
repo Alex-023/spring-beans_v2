2c783f1288155d3ff07e9ebe280a84882b6017a5
Alex <alexey0231@mail.ru>
2025-02-12 17:01:13 +0300

    v3.0

--- src/main/java/app/model/Cat.java
+++ src/main/java/app/model/Cat.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component("cat")
-@Primary
 public class Cat extends Animal {
     @Override
     public String toString() {
